1baa699ca46424d8aff8936a2346aacb64962502
c.pineda <c.pineda@unillanos.edu.co>
2026-05-12 09:45:24 -0500

    Implement unit tests for Product class methods

--- src/test/java/isi/shoppingCart/entities/ProductTest.java
+++ src/test/java/isi/shoppingCart/entities/ProductTest.java
@@ -16,29 +16,39 @@ class ProductTest {
 
     @Test
     void getId() {
+        assertEquals(1, product.getId());
     }
 
     @Test
     void getName() {
+        assertEquals("Laptop", product.getName());
     }
 
     @Test
     void getPrice() {
+        assertEquals(1000.0, product.getPrice(), 0.01);
     }
 
     @Test
     void getAvailableQuantity() {
+        assertEquals(10, product.getAvailableQuantity());
     }
 
     @Test
     void changePrice() {
+        product.changePrice(1500.0);
+        assertEquals(1500.0, product.getPrice());
     }
 
     @Test
     void increaseAvailableQuantity() {
+        product.increaseAvailableQuantity(5);
+        assertEquals(15, product.getAvailableQuantity());
     }
 
     @Test
     void decreaseAvailableQuantity() {
+        product.decreaseAvailableQuantity(5);
+        assertEquals(5, product.getAvailableQuantity());
     }
 }
